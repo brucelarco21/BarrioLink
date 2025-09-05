c67ae84df3ac0495ebe4bea4c69871eed0cc1622
brucelarco21 <br.larco@duocuc.cl>
2025-09-05 19:01:30 -0400

    fase1

--- Proyecto web/junta_vecinos/vecinos/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Vecino
-
-admin.site.register(Vecino)
--- Proyecto web/junta_vecinos/vecinos/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VecinosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vecinos'
